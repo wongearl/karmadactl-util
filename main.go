2275bcddb7ad5c6b8052aad5ed22c24877b2532e
wongearl <vamliang@163.com>
2023-05-25 11:06:22 +0800

    test: add test unjoin

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 
+	"github.com/wongearl/karmadactl-util/pkg/common"
 	"github.com/wongearl/karmadactl-util/pkg/join"
 	"github.com/wongearl/karmadactl-util/pkg/k8sclient"
+	"github.com/wongearl/karmadactl-util/pkg/unjoin"
 	"k8s.io/client-go/kubernetes"
 )
 
@@ -15,19 +17,42 @@ func main() {
 		return
 	}
 	testJoin(client)
+	testUnJoin(client)
 }
 
 func testJoin(k8sClient kubernetes.Interface) {
 	j := &join.Join{
-		ClusterName:                  "member1",
-		ClusterConfigSecretName:      "member-config",
-		ClusterConfigSecretNamespace: "default",
-		ClusterContext:               "kubernetes-admin@cluster.local",
-		KarmadaConfigSecretName:      "karmada-kubeconfig",
-		KarmadaConfigSecretNamespace: "karmada-system",
-		KarmadaContext:               "karmada-apiserver",
+		KarmdactlArg: common.KarmdactlArg{
+			ClusterName:                  "member1",
+			ClusterConfigSecretName:      "member-config",
+			ClusterConfigSecretNamespace: "default",
+			ClusterContext:               "kubernetes-admin@cluster.local",
+			KarmadaConfigSecretName:      "karmada-kubeconfig",
+			KarmadaConfigSecretNamespace: "karmada-system",
+			KarmadaContext:               "karmada-apiserver",
+		},
 	}
 	stdout, stderr, err := j.JoinCluster(k8sClient)
+	if err != nil {
+		fmt.Println("err:", err, "stderr:", stderr)
+		return
+	}
+	fmt.Println("stdout:", stdout)
+}
+
+func testUnJoin(k8sClient kubernetes.Interface) {
+	u := &unjoin.Unjoin{
+		KarmdactlArg: common.KarmdactlArg{
+			ClusterName:                  "member1",
+			ClusterConfigSecretName:      "member-config",
+			ClusterConfigSecretNamespace: "default",
+			ClusterContext:               "kubernetes-admin@cluster.local",
+			KarmadaConfigSecretName:      "karmada-kubeconfig",
+			KarmadaConfigSecretNamespace: "karmada-system",
+			KarmadaContext:               "karmada-apiserver",
+		},
+	}
+	stdout, stderr, err := u.JoinCluster(k8sClient)
 	if err != nil {
 		fmt.Println("err:", err, "stderr:", stderr)
 		return
