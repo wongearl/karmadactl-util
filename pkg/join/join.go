efa125cc9293e1471a4336bd7135b38c9ba1ff53
wongearl <vamliang@163.com>
2023-05-25 10:02:18 +0800

    docs: add karmada-system namespace

--- pkg/join/join.go
+++ pkg/join/join.go
@@ -16,15 +16,15 @@ type Join struct {
 	KarmadaContext               string
 }
 
-func JoinCluster(join Join, k8sClient kubernetes.Interface) (stdout, stderr []byte, err error) {
-	clusterKubeconfigPath, err := common.WriteKubeconfig("/tmp/clusterKubeconfig", join.ClusterConfigSecretName, join.ClusterConfigSecretNamespace, k8sClient)
+func (j *Join) JoinCluster(k8sClient kubernetes.Interface) (stdout, stderr []byte, err error) {
+	clusterKubeconfigPath, err := common.WriteKubeconfig("/tmp/clusterKubeconfig", j.ClusterConfigSecretName, j.ClusterConfigSecretNamespace, k8sClient)
 	if err != nil {
 		return nil, nil, err
 	}
-	karmadaKubeconfig, err := common.WriteKubeconfig("/tmp/karmadaKubeconfig", join.KarmadaConfigSecretName, join.KarmadaConfigSecretNamespace, k8sClient)
+	karmadaKubeconfig, err := common.WriteKubeconfig("/tmp/karmadaKubeconfig", j.KarmadaConfigSecretName, j.KarmadaConfigSecretNamespace, k8sClient)
 	if err != nil {
 		return nil, nil, err
 	}
-	return common.ExecAtLocal(filepath.Join("karmadactl"), "join", join.ClusterName, "--kubeconfig="+karmadaKubeconfig,
-		"--karmada-context="+join.KarmadaContext, "--cluster-kubeconfig="+clusterKubeconfigPath)
+	return common.ExecAtLocal(filepath.Join("karmadactl"), "join", j.ClusterName, "--kubeconfig="+karmadaKubeconfig,
+		"--karmada-context="+j.KarmadaContext, "--cluster-kubeconfig="+clusterKubeconfigPath)
 }
